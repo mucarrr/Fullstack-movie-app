a6d1175a9656ff773a98365334df918e65f26f6f
Merve Uçar <mucar2326@gmail.com>
2025-06-04 12:36:49 +0300

    end of the project

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -6,17 +6,15 @@ const Card = ({movie}) => {
     const r = movie.rating;
     const color = r > 9 ? 'bg-red-500' : r > 8 ? 'bg-green-500' : 'bg-yellow-500';
   return (
-    <Link to={`/movie/${movie.id}`} className='flex p-4 border shadow bg-white rounded-lg mx-auto max-w-[500px]  '>
-        <div className='relative w-1/2'>
+    <Link to={`/movie/${movie.id}`} className='relative flex p-4 border shadow bg-white rounded-lg mx-auto max-w-[500px]  '>
+        <div className='w-1/2'>
             <img src={movie.image} alt='poster' className='w-full h-full object-cover rounded-lg'></img>
             <span className={`absolute top-[-15px] right-[-15px] text-white px-2 py-1 rounded-full ${color} h-8 w-8 flex items-center justify-center`}>
                 {movie.rating}
             </span>
-            <span className='absolute bottom-0 left-0 bg-gray-900 text-white px-2 py-1 rounded-tr-md'>
-                {movie.year}
-            </span>
+            
         </div>
-        <div className="flex flex-col justify-between pl-6 w-1/2">
+        <div className="flex flex-col gap-10 justify-start pl-6 w-1/2">
             <h3 className='text-xl font-bold'>
                 {movie.title}
             </h3>
@@ -31,6 +29,9 @@ const Card = ({movie}) => {
                 <div className='text-sm bg-red-200 px-2 py-1 rounded-full max-w-[100px] text-center'>{movie.language}</div>
             </div>
         </div>
+        <span className='absolute bottom-4 right-4 bg-gray-900 text-white px-2 py-1 rounded-full'>
+                {movie.year}
+            </span>
     </Link>
         
    
